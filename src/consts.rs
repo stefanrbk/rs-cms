375c11bee2cc499e9561f484a9d650bb1d81fc7a
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-12 22:57:35 -0600

    Add consts and MAX_CHANNELS

---
+++ src/consts.rs
@@ -0,0 +1,1 @@
+pub const MAX_CHANNELS: usize = 16;
--- src/lib.rs
+++ src/lib.rs
@@ -19,5 +19,8 @@ pub type U16Fixed16Number = u32;
 pub type U1Fixed15Number = u16;
 pub type U8Fixed8Number = u16;
 
+mod consts;
 mod state;
 mod types;
+
+pub use consts::*;
