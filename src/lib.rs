a46cb1fe00b4c1a82cdf95f62153c2937c7beb7f
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-12 22:30:21 -0600

    Add an unsafe block macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
+#![deny(unsafe_code)]
 #![cfg_attr(debug_assertions, allow(unused_macros))]
 #![cfg_attr(debug_assertions, allow(unused_imports))]
 #![cfg_attr(debug_assertions, allow(dead_code))]
+
+/// Allow a block of 'unsafe' code with a reason.
+/// 
+/// The macro will expand to an 'unsafe' block.
+macro_rules! unsafe_block {
+    ($reason:tt => $body:expr) => {{
+        #[allow(unsafe_code)]
+        let r = unsafe { $body };
+        r
+    }};
+}
 
 mod state;
 mod types;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,1 +1,2 @@
 mod date_time;
+
