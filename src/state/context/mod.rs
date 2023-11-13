69a7029cf576ec7889f6427551528cd32f344278
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-12 23:13:55 -0600

    Add error handler to Context

--- src/state/context/mod.rs
+++ src/state/context/mod.rs
@@ -2,10 +2,13 @@ use std::{sync::Arc, any::Any};
 
 use crate::MAX_CHANNELS;
 
+use super::ErrorHandlerLogFunction;
+
 pub struct Context(Arc<ContextInner>);
 
 struct ContextInner {
     alarm_codes: [u16; MAX_CHANNELS],
     adaptation_state: f64,
     user_data: Box<dyn Any + Sync + Send>,
+    error_logger: Option<ErrorHandlerLogFunction>,
 }
