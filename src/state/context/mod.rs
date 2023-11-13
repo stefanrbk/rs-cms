ecc69083e8511dc2028ec744d6387831a957d69f
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-12 23:02:26 -0600

    Add a few field to ContextInner

--- src/state/context/mod.rs
+++ src/state/context/mod.rs
@@ -1,5 +1,11 @@
-use std::sync::Arc;
+use std::{sync::Arc, any::Any};
+
+use crate::MAX_CHANNELS;
 
 pub struct Context(Arc<ContextInner>);
 
-struct ContextInner {}
+struct ContextInner {
+    alarm_codes: [u16; MAX_CHANNELS],
+    adaptation_state: f64,
+    user_data: Box<dyn Any + Sync + Send>,
+}
