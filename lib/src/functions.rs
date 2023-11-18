332f8fa047522e07b997ec11b2e67c1b65e51886
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-18 17:40:58 -0600

    Add some inline functions

--- lib/src/functions.rs
+++ lib/src/functions.rs
@@ -1,6 +1,8 @@
+use std::mem::size_of;
+
 use chrono::{DateTime, Utc, TimeZone, Timelike, Datelike};
 
-use crate::{types::DateTimeNumber, S15Fixed16Number, U16Fixed16Number, U8Fixed8Number};
+use crate::{types::DateTimeNumber, S15Fixed16Number, U16Fixed16Number, U8Fixed8Number, PTR_ALIGNMENT};
 
 #[inline]
 pub fn u8_fixed8_number_to_f64(fixed8: U8Fixed8Number) -> f64 {
@@ -69,4 +71,80 @@ pub fn decode_date_time(source: DateTimeNumber) -> DateTime<Utc> {
         source.seconds.to_be() as u32,
     )
     .unwrap()
+}
+
+#[inline]
+pub const fn align_long(x: usize) -> usize {
+    (x + (size_of::<u32>() - 1)) & !(size_of::<u32>() - 1)
+}
+#[inline]
+pub const fn align_mem(x: usize) -> usize {
+    x + (PTR_ALIGNMENT - 1) & !(PTR_ALIGNMENT - 1)
+}
+
+#[inline]
+pub const fn from_8_to_16(rgb: u8) -> u16 {
+    ((rgb as u16) << 8) | rgb as u16
+}
+
+#[inline]
+pub const fn from_16_to_8(rgb: u16) -> u8 {
+    ((((rgb as u32) * 65281 + 8388608) >> 24) & 0xFF) as u8
+}
+
+#[inline]
+pub const fn fixed_to_int(x: S15Fixed16Number) -> i32 {
+    x >> 16
+}
+
+#[inline]
+pub const fn fixed_rest_to_int(x: S15Fixed16Number) -> i32 {
+    x & 0xFFFF
+}
+
+#[inline]
+pub const fn round_fixed_to_int(x: S15Fixed16Number) -> i32 {
+    (x + 0x8000) >> 16
+}
+
+#[inline]
+pub const fn to_fixed_domain(a: i32) -> S15Fixed16Number {
+    a + ((a + 0x7fff) / 0xffff)
+}
+
+#[inline]
+pub const fn from_fixed_domain(a: S15Fixed16Number) -> i32 {
+    a - ((a + 0x7fff) >> 16)
+}
+
+#[inline]
+pub const fn quick_floor(val: f64) -> i32 {
+    const DOUBLE_2_FIX_MAGIC: f64 = 68719476736.0 * 1.5;
+
+    union Split {
+        pub val: f64,
+        pub halves: [i32; 2],
+    }
+
+    let i = Split { val };
+
+    unsafe_block!("Accessing part of a union for performing quick_floor" => i.halves[0]) 
+}
+
+#[inline]
+pub fn quick_floor_word(d: f64) -> u16 {
+    (quick_floor(d - 32767.0) + 32767) as u16
+}
+
+#[inline]
+pub fn quick_saturate_word(d: f64) -> u16 {
+    let d = d + 0.5;
+    if d <= 0.0 {
+        return 0;
+    }
+    if d >= 65535.0 {
+        return 0xffff;
+    }
+
+    quick_floor_word(d)
 }
