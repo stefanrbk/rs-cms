b74f2339f2abfe0c726a902c7e8fd02cb4cb0aaf
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-18 17:40:32 -0600

    Add a PTR_ALIGNMENT const

--- lib/src/consts.rs
+++ lib/src/consts.rs
@@ -1,3 +1,7 @@
+use std::mem::size_of;
+
 pub const MAX_CHANNELS: usize = 16;
 pub const MAX_INPUT_DIMENSIONS: usize = 15;
 pub const MAX_TYPES_IN_PLUGIN: usize = 20;
+
+pub const PTR_ALIGNMENT: usize = size_of::<usize>();
