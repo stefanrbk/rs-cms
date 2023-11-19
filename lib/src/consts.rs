86fa861bbe511e072d0e080a1712d34ed376d824
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-18 22:26:31 -0600

    Add SemVer support and set the library VERSION to 0.1.0

--- lib/src/consts.rs
+++ lib/src/consts.rs
@@ -1,5 +1,9 @@
 use std::mem::size_of;
 
+use crate::SemVer;
+
+pub const VERSION: SemVer = SemVer::new(0, 1, 0);
+
 pub const MAX_CHANNELS: usize = 16;
 pub const MAX_INPUT_DIMENSIONS: usize = 15;
 pub const MAX_TYPES_IN_PLUGIN: usize = 20;
