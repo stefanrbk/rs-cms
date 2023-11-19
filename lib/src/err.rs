9bea85434cc623c5275e49ef3afcb09be20c4fcd
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-18 22:19:57 -0600

    Move err macro to its own mod

---
+++ lib/src/err.rs
@@ -0,0 +1,58 @@
+macro_rules! err {
+    ($c:expr, $l:ident, $e:ident, $s:tt; str => $ss:tt) => { {
+                $c.signal_error(
+                log::Level::$l,
+                crate::state::ErrorCode::$e,
+                &$s
+            );
+            Err($ss)
+        }
+    };
+    ($c:expr, $l:ident, $e:ident, $s:tt, $($exprs:expr),*; str => $ss:tt) => { {
+                $c.signal_error(
+                log::Level::$l,
+                crate::state::ErrorCode::$e,
+                &format!($s, $($exprs),*)
+            );
+            Err($ss)
+        }
+    };
+    (io => $t:ident, $s:tt) => {
+        Err(std::io::Error::new(std::io::ErrorKind::$t, $s))
+    };
+    ($c:expr, $l:ident, $e:ident, $s:tt; io => $t:ident, $ss:tt) => {{
+        $c.signal_error(
+            log::Level::$l,
+            crate::state::ErrorCode::$e,
+            $s
+        );
+        Err(std::io::Error::new(std::io::ErrorKind::$t, $ss))}
+    };
+    ($c:expr, $l:ident, $e:ident, $s:tt; io => $error:expr) => { {
+                $c.signal_error(
+                log::Level::$l,
+                crate::state::ErrorCode::$e,
+                &$s
+            );
+            Err($error)
+        }
+    };
+    ($c:expr, $l:ident, $e:ident, $s:tt, $($exprs:expr),*; io => $t:ident, $ss:tt) => { {
+            $c.signal_error(
+                log::Level::$l,
+                crate::state::ErrorCode::$e,
+                &format!($s, $($exprs),*)
+            );
+            Err(std::io::Error::new(std::io::ErrorKind::$t, $ss))
+        }
+    };
+    ($c:expr, $l:ident, $e:ident, $s:tt, $($exprs:expr),*; io => $error:expr) => { {
+                $c.signal_error(
+                log::Level::$l,
+                crate::state::ErrorCode::$e,
+                &format!($s, $($exprs),*)
+            );
+            Err($error)
+        }
+    };
+}
