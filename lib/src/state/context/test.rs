4783d23110b475e53d2688a5e54baff9717aa00b
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-21 05:10:48 -0600

    Refactor function and add new plugin definition
    - The code has been refactored to import additional modules and types.
    - A new test case has been added to register an interpolation plugin.
    - The implementation of the `TEST_INTERP` function has been updated.
    - A new static plugin definition (`TEST_INTERP_PLUGIN`) has been added.

--- lib/src/state/context/test.rs
+++ lib/src/state/context/test.rs
@@ -1,16 +1,21 @@
-use crate::{Result, DEFAULT_CONTEXT, plugin::{Plugin, InterpFnFactory}};
+use crate::{
+    plugin::{InterpFnFactory, Plugin, TagTypeHandler},
+    types::Signature,
+    Result, DEFAULT_CONTEXT,
+};
 
 #[test]
 fn register_interp_plugin_succeeds() -> Result<()> {
     let context = DEFAULT_CONTEXT.register_plugins(&[&TEST_INTERP_PLUGIN]);
     if let Ok(ref ctx) = context {
         if ctx.0.interp_factory == TEST_INTERP {
-        return Ok(())
-    }
+            return Ok(());
+        }
     } else {
-        return Err(context.err().unwrap())}
+        return Err(context.err().unwrap());
+    }
     Err("Failed to register plugin")
 }
 
-static TEST_INTERP: InterpFnFactory = |_,_,_| panic!("This function should never run!!!");
+static TEST_INTERP: InterpFnFactory = |_, _, _| panic!("This function should never run!!!");
 static TEST_INTERP_PLUGIN: Plugin = Plugin::create_interpolation_plugin(&TEST_INTERP);
