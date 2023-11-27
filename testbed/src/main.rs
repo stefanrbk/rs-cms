a4eee0a60283c2f436dded5bce15010de9ade81d
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-27 01:45:05 -0600

    feat: Add error logger with custom handler
    - Adds an error logger with a custom handler to handle errors in the
    `rs-cms` test bed.
    - The custom handler logs errors using the default error handler log
    function.
    - If the error level is greater than or equal to the error level, the
    program exits with code 1.

--- testbed/src/main.rs
+++ testbed/src/main.rs
@@ -1,5 +1,8 @@
+use std::process::exit;
+
 use clap::Parser;
-use log::{info, Level};
+use log::{info, log, Level};
+use rs_cms::state::{default_error_handler_log_function, Context, ErrorCode};
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -22,4 +25,16 @@ pub fn main() {
     simple_logger::init_with_level(Level::Info).unwrap();
 
     info!("rs-cms {} test bed", rs_cms::VERSION);
+
+    info!("Installing error logger ...");
+    rs_cms::state::DEFAULT_CONTEXT.set_error_logger(Some(die));
+    info!("done.");
+}
+
+fn die(context_id: &Context, level: Level, error_code: ErrorCode, text: &str) {
+    default_error_handler_log_function(context_id, level, error_code, text);
+
+    if level >= Level::Error {
+        exit(1);
+    }
 }
