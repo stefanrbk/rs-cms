2c16cc92f4e2d20ffe74cd7c5155a5e6685cae68
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-27 01:58:44 -0600

    Add 'print_supported_intents' function to main.rs
    - Adds a new function called 'print_supported_intents' to the main.rs
    file.
    - The function retrieves supported intents from DEFAULT_CONTEXT and
    displays them using the log crate.
    - This function is called at the end of the 'main' function.
    - Supports up to 200 intents.

--- testbed/src/main.rs
+++ testbed/src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use log::{info, Level};
 use rs_cms::state::{
     default_error_handler_log_function, Context, ErrorCode, ErrorHandlerLogFunction,
+    DEFAULT_CONTEXT,
 };
 
 #[derive(Parser)]
@@ -33,6 +34,8 @@ pub fn main() {
     info!("Installing error logger ...");
     rs_cms::state::DEFAULT_CONTEXT.set_error_logger(FATAL_ERROR_QUIT);
     info!("done.");
+
+    print_supported_intents();
 }
 
 fn die(context_id: &Context, level: Level, error_code: ErrorCode, text: &str) {
@@ -40,5 +43,16 @@ fn die(context_id: &Context, level: Level, error_code: ErrorCode, text: &str) {
 
     if level >= Level::Error {
         exit(1);
+    }
+}
+
+fn print_supported_intents() {
+    let mut intents: Vec<(u32, &str)> = Vec::with_capacity(200);
+
+    let n = DEFAULT_CONTEXT.get_supported_intents(200, &mut intents);
+
+    info!("Supported intents:");
+    for i in 0..n {
+        info!("\t{} - {}", intents[i].0, intents[i].1);
     }
 }
