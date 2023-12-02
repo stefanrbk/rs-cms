7e580a50073b0b0add6506dcec500e3a6ec6e544
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-12-02 00:49:18 -0600

    Add constant for XYZ color values
    Add a new constant `D50` for XYZ color values in the `consts.rs` module.
    This constant represents the XYZ color values for the D50 illuminant. It
    is initialized with the specific x, y, and z values associated with D50.

--- lib/src/consts.rs
+++ lib/src/consts.rs
@@ -2,9 +2,15 @@ use std::mem::size_of;
 
 use once_cell::sync::Lazy;
 
-use crate::{SemVer, state::Context};
+use crate::{state::Context, types::XYZ, SemVer};
 
 pub const VERSION: SemVer = SemVer::new(0, 1, 0);
+
+pub const D50: XYZ = XYZ {
+    x: 0.9642,
+    y: 1.0,
+    z: 0.8249,
+};
 
 pub const MAX_CHANNELS: usize = 16;
 pub const MAX_INPUT_DIMENSIONS: usize = 15;
