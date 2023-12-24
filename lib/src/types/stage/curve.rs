aaacc2bdca9a2cadb98fc8c36efb5061fccb2024
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-12-23 18:29:06 -0600

    Add get_curve_set for Stage

--- lib/src/types/stage/curve.rs
+++ lib/src/types/stage/curve.rs
@@ -1,6 +1,6 @@
 use crate::{types::curve::Curve, state::Context};
 
 pub struct StageCurve {
-    pub curves: Box<Curve>,
+    pub curves: Box<[Curve]>,
     pub context_id: Context,
 }
--- lib/src/types/stage/mod.rs
+++ lib/src/types/stage/mod.rs
@@ -1,8 +1,8 @@
 use std::any::Any;
 
-use crate::state::Context;
+use crate::{state::Context, types::stage::curve::StageCurve};
 
-use super::Signature;
+use super::{Signature, curve::Curve};
 
 pub type StageEvalFn = fn(r#in: &[f32], out: &mut [f32], stage: &Stage);
 pub type StageDupFn = fn(stage: &Stage) -> Stage;
@@ -19,3 +19,22 @@ pub struct Stage {
 }
 
 mod curve;
+
+impl Stage {
+    pub(crate) fn get_curve_set(&self) -> Option<&[Curve]> {
+        if let Some(data) = self.data.downcast_ref::<StageCurve>() {
+            Some(&data.curves)
+        } else {
+            None
+        }
+    }
+
+    // fn eval_curves(&self, r#in: &[f32], out: &mut [f32]) {
+    //     if let Some(data) = self.get_curve_set() {
+    //         let n_curves = data.len();
+    //         for i in 0..n_curves {
+    //             out[i] = data[i].
+    //         }
+    //     }
+    // }
+}
