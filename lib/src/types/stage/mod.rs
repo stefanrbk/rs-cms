7cc8bdfe2a792dd1c0862895b7c6be53717dc116
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-12-26 02:22:14 -0600

    Add Stage dup_curve_set, new_curves, and new_identity_curves

--- lib/src/types/stage/mod.rs
+++ lib/src/types/stage/mod.rs
@@ -1,6 +1,6 @@
 use std::any::Any;
 
-use crate::{state::Context, types::stage::curve::StageCurve, Result};
+use crate::{sig, state::Context, types::stage::curve::StageCurve, Result};
 
 use super::{curve::Curve, Signature};
 
@@ -36,5 +36,60 @@ impl Stage {
                 out[i] = data[i].eval_f32(r#in[i]);
             }
         }
+    }
+
+    fn dup_curve_set(&self) -> Result<Box<dyn Any>> {
+        let data = self
+            .data
+            .downcast_ref::<StageCurve>()
+            .ok_or("Stage is not a Curve")?;
+
+        Ok(Box::new(StageCurve {
+            curves: data
+                .curves
+                .iter()
+                .map(|x| x.dup().unwrap())
+                .collect::<Vec<Curve>>()
+                .into(),
+            context_id: data.context_id.clone(),
+        }))
+    }
+
+    pub fn new_curves(context_id: &Context, curves: &[Curve]) -> Result<Self> {
+        let num_chans = curves.len();
+        let new_elem = StageCurve {
+            curves: curves.into(),
+            context_id: context_id.clone(),
+        };
+
+        Ok(Self {
+            context_id: context_id.clone(),
+            r#type: sig::mpe_stage::CURVE_SET,
+            implements: sig::mpe_stage::CURVE_SET,
+            in_chans: num_chans,
+            out_chans: num_chans,
+            eval: Self::eval_curves,
+            dup: Self::dup_curve_set,
+            data: Box::new(new_elem),
+        })
+    }
+
+    pub fn new_identity_curves(context_id: &Context, num_chans: usize) -> Result<Self> {
+        let curves = vec![Curve::build_gamma(context_id, 1f64).unwrap(); num_chans];
+        let new_elem = StageCurve {
+            curves: curves.into_boxed_slice(),
+            context_id: context_id.clone(),
+        };
+
+        Ok(Self {
+            context_id: context_id.clone(),
+            r#type: sig::mpe_stage::CURVE_SET,
+            implements: sig::mpe_stage::IDENTITY,
+            in_chans: num_chans,
+            out_chans: num_chans,
+            eval: Self::eval_curves,
+            dup: Self::dup_curve_set,
+            data: Box::new(new_elem),
+        })
     }
 }
