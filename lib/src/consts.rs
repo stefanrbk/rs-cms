a6fb3949b0515043427ca9977bb15072ed3e0bd5
Stefan Kewatt <stefan.kewatt@gmail.com>
2024-01-07 02:11:05 -0600

    Add const flags

--- lib/src/consts.rs
+++ lib/src/consts.rs
@@ -26,3 +26,26 @@ pub(crate) const MINUS_INF: f64 = -1e22f64;
 pub(crate) const PLUS_INF: f64 = 1e22f64;
 
 pub const SAMPLER_INSPECT: u32 = 0x01000000;
+pub mod flags {
+    pub const NO_CACHE: u32 = 0x0040;
+    pub const NO_OPTIMIZE: u32 = 0x0100;
+    pub const NULL_TRANSFORM: u32 = 0x0200;
+    pub const GAMUT_CHECK: u32 = 0x1000;
+    pub const SOFT_PROOFING: u32 = 0x4000;
+    pub const BLACK_POINT_COMPENSATION: u32 = 0x2000;
+    pub const NO_WHITE_ON_WHITE_FIXUP: u32 = 0x0004;
+    pub const HIGH_RES_PRECALC: u32 = 0x0400;
+    pub const LOW_RES_PRECALC: u32 = 0x0800;
+    pub const BITS_8_DEVICE_LINK: u32 = 0x0008;
+    pub const GUESS_DEVICE_CLASS: u32 = 0x0020;
+    pub const KEEP_SEQUENCE: u32 = 0x0080;
+    pub const FORCE_CLUT: u32 = 0x0002;
+    pub const CLUT_POST_LINEARIZATION: u32 = 0x0001;
+    pub const CLUT_PRE_LINEARIZATION: u32 = 0x0010;
+    pub const NO_NEGATIVES: u32 = 0x8000;
+    pub const COPY_ALPHA: u32 = 0x04000000;
+    pub const NO_DEFAULT_RESOURCE_DEF: u32 = 0x01000000;
+    pub const fn grid_points(n: usize) -> usize {
+        (n & 0xff) << 16
+    }
+}
