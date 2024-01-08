3fb124d38af966903c9f61660970d93b8c89bea4
Stefan Kewatt <stefan.kewatt@gmail.com>
2024-01-07 22:14:50 -0600

    Add 2 helper functions for colorspaces

--- lib/src/colorspace.rs
+++ lib/src/colorspace.rs
@@ -1,5 +1,22 @@
-use crate::{err, sig, types::Signature, Result};
+use crate::{
+    err,
+    flags::{HIGH_RES_PRECALC, LOW_RES_PRECALC},
+    sig,
+    types::Signature,
+    Result,
+};
 use sig::colorspace::*;
+
+const RGB_BLACK: [u16; 3] = [0, 0, 0];
+const RGB_WHITE: [u16; 3] = [0xffff, 0xffff, 0xffff];
+const CMYK_BLACK: [u16; 4] = [0xffff, 0xffff, 0xffff, 0xffff];
+const CMYK_WHITE: [u16; 4] = [0, 0, 0, 0];
+const LAB_BLACK: [u16; 3] = [0, 0x8080, 0x8080];
+const LAB_WHITE: [u16; 3] = [0xffff, 0x8080, 0x8080];
+const CMY_BLACK: [u16; 3] = [0xffff, 0xffff, 0xffff];
+const CMY_WHITE: [u16; 3] = [0, 0, 0];
+const GRAY_BLACK: [u16; 1] = [0];
+const GRAY_WHITE: [u16; 1] = [0xffff];
 
 pub fn channels_of_colorspace(colorspace: Signature) -> Result<usize> {
     Ok(match colorspace {
@@ -24,4 +41,45 @@ pub fn channels_of_colorspace(colorspace: Signature) -> Result<usize> {
 
 pub(crate) fn channels_of(colorspace: Signature) -> usize {
     channels_of_colorspace(colorspace).unwrap_or(3)
+}
+
+pub(crate) fn reasonable_gridpoints_by_colorspace(colorspace: Signature, flags: u32) -> usize {
+    // Already specified?
+    if flags & 0x00ff0000 != 0 {
+        // Yes, grab'em
+        return ((flags >> 16) & 0xff) as usize;
+    }
+
+    let n_chan = channels_of(colorspace);
+
+    // HIGH_RES_PRECALC is maximum resolution
+    // LOW_RES_PRECALC is lower resolution
+    match (
+        flags & HIGH_RES_PRECALC != 0,
+        flags & LOW_RES_PRECALC != 0,
+        n_chan,
+    ) {
+        (_, false, 5..) => 7, // 7 for Hifi
+        (true, _, 4) => 23,   // 23 for CMYK
+        (true, _, _) => 49,   // 49 for RGB and others
+        (_, true, 5..) => 6,  // 6 for more than 4 channels
+        (_, true, 1) => 33,   // For monochrome
+        (_, true, _) => 17,   // 17 for remaining
+        (_, _, 4) => 17,      // 17 for CMYK
+        _ => 33,              // 33 for RGB
+    }
+}
+
+pub(crate) fn end_points_by_space(
+    space: Signature,
+) -> Option<(&'static [u16], &'static [u16], usize)> {
+    // Only most common spaces
+    Some(match space {
+        GRAY => (&GRAY_WHITE, &GRAY_BLACK, 1),
+        RGB => (&RGB_WHITE, &RGB_BLACK, 3),
+        LAB => (&LAB_WHITE, &LAB_BLACK, 3),
+        CMY => (&CMY_WHITE, &CMY_BLACK, 3),
+        CMYK => (&CMYK_WHITE, &CMYK_BLACK, 4),
+        _ => return None,
+    })
 }
