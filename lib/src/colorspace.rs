39f7d04def4277a5a8f6d53fec7407e6c24eb493
Stefan Kewatt <stefan.kewatt@gmail.com>
2024-01-07 22:33:58 -0600

    Add conversion fns to go between rcms notation and icc notation

--- lib/src/colorspace.rs
+++ lib/src/colorspace.rs
@@ -5,7 +5,6 @@ use crate::{
     types::Signature,
     Result,
 };
-use sig::colorspace::*;
 
 const RGB_BLACK: [u16; 3] = [0, 0, 0];
 const RGB_WHITE: [u16; 3] = [0xffff, 0xffff, 0xffff];
@@ -19,6 +18,8 @@ const GRAY_BLACK: [u16; 1] = [0];
 const GRAY_WHITE: [u16; 1] = [0xffff];
 
 pub fn channels_of_colorspace(colorspace: Signature) -> Result<usize> {
+    use sig::colorspace::*;
+
     Ok(match colorspace {
         MCH1 | COLOR1 | GRAY => 1,
         MCH2 | COLOR2 => 2,
@@ -73,6 +74,8 @@ pub(crate) fn reasonable_gridpoints_by_colorspace(colorspace: Signature, flags: 
 pub(crate) fn end_points_by_space(
     space: Signature,
 ) -> Option<(&'static [u16], &'static [u16], usize)> {
+    use sig::colorspace::*;
+    
     // Only most common spaces
     Some(match space {
         GRAY => (&GRAY_WHITE, &GRAY_BLACK, 1),
@@ -80,6 +83,78 @@ pub(crate) fn end_points_by_space(
         LAB => (&LAB_WHITE, &LAB_BLACK, 3),
         CMY => (&CMY_WHITE, &CMY_BLACK, 3),
         CMYK => (&CMYK_WHITE, &CMYK_BLACK, 4),
+        _ => return None,
+    })
+}
+
+pub(crate) fn icc_colorspace(our_notation: u32) -> Option<Signature> {
+    use crate::types::pixel_type as pt;
+    use sig::colorspace as sig;
+
+    Some(match our_notation {
+        1 | pt::GRAY => sig::GRAY,
+        2 | pt::RGB => sig::RGB,
+        pt::CMY => sig::CMY,
+        pt::CMYK => sig::CMYK,
+        pt::YCB_CR => sig::YCBCR,
+        pt::YUV => sig::LUV,
+        pt::XYZ => sig::XYZ,
+        pt::LAB | pt::LAB_V2 => sig::LAB,
+        pt::YUVK => sig::LUVK,
+        pt::HSV => sig::HSV,
+        pt::HLS => sig::HLS,
+        pt::YXY => sig::YXY,
+        pt::MCH1 => sig::MCH1,
+        pt::MCH2 => sig::MCH2,
+        pt::MCH3 => sig::MCH3,
+        pt::MCH4 => sig::MCH4,
+        pt::MCH5 => sig::MCH5,
+        pt::MCH6 => sig::MCH6,
+        pt::MCH7 => sig::MCH7,
+        pt::MCH8 => sig::MCH8,
+        pt::MCH9 => sig::MCH9,
+        pt::MCH10 => sig::MCHA,
+        pt::MCH11 => sig::MCHB,
+        pt::MCH12 => sig::MCHC,
+        pt::MCH13 => sig::MCHD,
+        pt::MCH14 => sig::MCHE,
+        pt::MCH15 => sig::MCHF,
+        _ => return None,
+    })
+}
+
+pub(crate) fn rcms_colorspace(profile_space: Signature) -> Option<u32> {
+    use crate::types::pixel_type as pt;
+    use sig::colorspace as sig;
+
+    Some(match profile_space {
+        sig::GRAY => pt::GRAY,
+        sig::RGB => pt::RGB,
+        sig::CMY => pt::CMY,
+        sig::CMYK => pt::CMYK,
+        sig::YCBCR => pt::YCB_CR,
+        sig::LUV => pt::YUV,
+        sig::XYZ => pt::XYZ,
+        sig::LAB => pt::LAB,
+        sig::LUVK => pt::YUVK,
+        sig::HSV => pt::HSV,
+        sig::HLS => pt::HLS,
+        sig::YXY => pt::YXY,
+        sig::MCH1 => pt::MCH1,
+        sig::MCH2 => pt::MCH2,
+        sig::MCH3 => pt::MCH3,
+        sig::MCH4 => pt::MCH4,
+        sig::MCH5 => pt::MCH5,
+        sig::MCH6 => pt::MCH6,
+        sig::MCH7 => pt::MCH7,
+        sig::MCH8 => pt::MCH8,
+        sig::MCH9 => pt::MCH9,
+        sig::MCHA => pt::MCH10,
+        sig::MCHB => pt::MCH11,
+        sig::MCHC => pt::MCH12,
+        sig::MCHD => pt::MCH13,
+        sig::MCHE => pt::MCH14,
+        sig::MCHF => pt::MCH15,
         _ => return None,
     })
 }
